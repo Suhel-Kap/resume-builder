8d016556c849759adbc33a17391ad931e24f83e1
Suhel-Kap <suhelkapadia2@gmail.com>
2022-02-04 15:12:01 +0530

    Add experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,8 +1,10 @@
 class ExperiencesController < ApplicationController
-    before_action: logged_in_user, only: [:new]
+    before_action :logged_in_user, only: [:new]
 
     def new
         current_user.profile.experiences.create
-        flash[:success] = "Experience added."
+        flash[:success] = "experience added."
         redirect_to edit_url
     end
+
+end
