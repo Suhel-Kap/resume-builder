ee73280e9de676d03c59ebb0d393084ecdfe34ff
Suhel-Kap <suhelkapadia2@gmail.com>
2022-02-04 14:51:21 +0530

    Add experiences

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   resources :profiles
   resources :educations , only: [:new]
+  resources :experiences , only: [:new]
 end
--- db/migrate/20220203110911_add_experience_to_profiles.rb
+++ db/migrate/20220203110911_add_experience_to_profiles.rb
@@ -1,5 +1,3 @@
 class AddExperienceToProfiles < ActiveRecord::Migration[6.1]
-  def change
     add_reference :profiles, :profile, null: true, foreign_key: true
-  end
 end
