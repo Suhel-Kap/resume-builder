cc27c6f38d539194255e82dbe2f8bec99ad69d9b
Suhel-Kap <suhelkapadia2@gmail.com>
2022-02-04 16:19:42 +0530

    Add projects

--- db/migrate/20220204104543_add_experience_id_to_projects.rb
+++ db/migrate/20220204104543_add_experience_id_to_projects.rb
@@ -1,6 +1,5 @@
 class AddExperienceIdToProjects < ActiveRecord::Migration[6.1]
   def change
-    add_column :projects, :experience_id, :integer
     add_reference :experiences, :experience, null: true, foreign_key: true
   end
 end
